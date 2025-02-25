6421e143570882860839c877444a5207b72bfde3
Kishukumar07 <kishukumars082@gmail.com>
2025-02-26 02:41:37 +0530

    added login route features

--- controllers/user.js
+++ controllers/user.js
@@ -1,58 +1,85 @@
-const express = require('express')
-const userRoutes = express.Router()
-const { Usermodel } = require('../models/user')
-const bcrypt = require('bcrypt')
+const express = require('express');
+const userRoutes = express.Router();
+const { Usermodel } = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv=require('dotenv'); 
+dotenv.config()
 
 
 userRoutes.post("/register", async (req, res) => {
     try {
         const { email, password, name, age, city } = req.body;
 
-        // Check if all fields are provided
         if (!email || !password || !name || !age || !city) {
-            return res.status(400).json({ msg: "All fields are required: name, email, password, age, and city." });
+            return res.status(400).json({ msg: "All fields are required: Name, Email, Password, Age, and City." });
         }
 
-        // Check if user already exists
         const existingUser = await Usermodel.findOne({ email });
         if (existingUser) {
-            return res.status(409).json({ msg: "User already exists" });
+            return res.status(409).json({ msg: "User already exists." });
         }
 
-        //hashing password with bcrypt -sequrity feature  
         bcrypt.hash(password, 5, async (err, hash) => {
-            // Store hash in your password DB.
-            // Create new user
-            const user = new Usermodel({ name, email, "password": hash, age, city });
-            await user.save();
+            if (err) {
+                return res.status(500).json({ msg: "Internal server error." });
+            }
+            try {
+
+                const user = new Usermodel({ name, email, "password": hash, age, city });
+                await user.save();
+
+                res.status(201).json({ msg: "User registered successfully!" });
+
+            } catch (err) {
+
+                res.status(500).json({ msg: "Internal server error.", error: err.message });
+            }
         });
 
-        res.status(201).json({ msg:"User registered successfully!" });
-
     } catch (err) {
-        res.status(500).json({ msg: "Internal Server Error", error: err.message });
+
+        res.status(500).json({ msg: "Internal server error.", error: err.message });
     }
 });
 
 
 
 
+userRoutes.post("/login", async (req, res) => {
+    // Finding the user with email
+    const { email, password } = req.body;
 
+    try {
+        const user = await Usermodel.findOne({ email });
 
+        if (!user) {
+            return res.status(401).json({ msg: "Wrong credentials." });
+        }
 
+        const hashedPassword = user.password;
 
+        bcrypt.compare(password, hashedPassword, (err, result) => { // Can also use this with await keyword, no need for a callback
+            // result == true
+            if (err) {
+                return res.status(500).json({ msg: "Internal server error." });
+            }
 
-userRoutes.post("/login", (req, res) => {
-    //logicwill be here 
-    res.send("login route ")
+            if (!result) {
+                return res.status(401).json({ msg: "Wrong credentials." });
+            }
 
+            // Returning response with token
+            const token = jwt.sign({ foo: 'bar' }, process.env.jwtSecretKey);
+            res.status(200).send("Login successful.");
 
-})
+        });
 
-
-
-
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+});
 
 module.exports = {
     userRoutes
-}
+};
