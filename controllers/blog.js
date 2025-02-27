9f5ef84126eaa50ebb553ece2b267dd1a063fca6
Kishukumar07 <kishukumars082@gmail.com>
2025-02-27 19:53:48 +0530

    patch and delete blog route done

--- controllers/blog.js
+++ controllers/blog.js
@@ -33,15 +33,53 @@ blogRoutes.get("/", async (req, res) => {
 
 
 
-// the delete route to allow only the author to delete the blog. 
-blogRoutes.delete("/delete/:blogID", async (req, res) => {
-
+//Function: the update route to allow only the author to update the blog. 
+blogRoutes.patch("/update/:blogID", async (req, res) => {
     try {
         const { blogID } = req.params
         const authorID = req.body.authorID; // Authenticated User ID from token
 
+
+
+
         // Find the blog
+
         const blog = await Blogmodel.findById(blogID);
+
+
+
+
+
+        if (!blog) return res.status(404).json({ "msg": "Blog not found" });
+
+        // Ensure only the author can delete
+        if (blog.authorID != authorID) {
+            return res.status(403).json({ "msg": "Unauthorized to Update this blog" });
+        }
+
+        await Blogmodel.findByIdAndUpdate({ _id: blogID }, req.body)
+        res.status(200).send({ "msg": `the note with id${blogID} has been Updated` })
+
+        //   res.status(200).json({ "msg": "Blog deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ "msg": err.message });
+    }
+
+});
+
+
+
+
+//Function: the delete route to allow only the author to delete the blog. 
+blogRoutes.delete("/delete/:blogID", async (req, res) => {
+    try {
+        const { blogID } = req.params
+        const authorID = req.body.authorID; // Authenticated User ID from token
+
+        // Find the blog    
+        const blog = await Blogmodel.findById(blogID);
+
+
         if (!blog) return res.status(404).json({ "msg": "Blog not found" });
 
         // Ensure only the author can delete
@@ -52,18 +90,12 @@ blogRoutes.delete("/delete/:blogID", async (req, res) => {
         await Blogmodel.findByIdAndDelete({ _id: blogID })
         res.status(200).send({ "msg": `the note with id${blogID} has been Deleted` })
 
-        //   res.status(200).json({ "msg": "Blog deleted successfully" });
+        // res.status(200).json({ "msg": "Blog deleted successfully" });
     } catch (err) {
         res.status(500).json({ "msg": err.message });
     }
 
-
 });
-
-
-
-
-
 
 
 
