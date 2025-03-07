3c409eed114175c6407cd8b0430e96a3e135c383
Kishukumar07 <kishukumars082@gmail.com>
2025-03-07 18:58:44 +0530

    setting up the node mailer

---
+++ nodemailer/index.js
@@ -0,0 +1,28 @@
+const nodemailer =require('nodemailer'); 
+const env =require('dotenv'); 
+env.config()
+
+//creating the transpoter 
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",         
+    port: 587,           
+    secure: false, // true for port 465, false for other ports
+    auth: {
+      user: process.env.USER_EMAIL, 
+      pass: process.env.PASS,
+    },
+  });
+
+
+// async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: 'lovishnik@gmail.com', // sender address
+      to: "kishukumars082@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+//   }
