e7839445e499675bb648a223dd313bd226c05c66
Kishukumar07 <kishukumars082@gmail.com>
2025-03-12 15:16:40 +0530

    created new event with .emit

--- socket.io/index.js
+++ socket.io/index.js
@@ -14,12 +14,15 @@ const io = new Server(httpServer);
 io.on('connection', (socket) => {
     console.log('User connected');
 
-    socket.on('disconnect',()=>{
-        console.log("User disconnected");  
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
     })  //if we refresh the client ui its get disconnected and then get connected again....
- 
-    socket.send("Welcome to my world!")
-  
+
+    socket.send("Welcome to my world!");
+    socket.emit("newEvent", "This is a new Event "); //this is how we can create custom events as well ....
+
+
+
 });
 
 httpServer.listen(3000, () => {
