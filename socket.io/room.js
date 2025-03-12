e667687ace75fe5107d8f300521ee4a32ed3a456
Kishukumar07 <kishukumars082@gmail.com>
2025-03-12 20:35:16 +0530

    rooms with total member

--- socket.io/room.js
+++ socket.io/room.js
@@ -10,24 +10,48 @@ const io = new Server(httpServer);
 
 
 //creating three different rooms...
-const rooms =['Room 1','Room 2','Room 3'];  
+// const rooms =['Room 1','Room 2','Room 3'];  
 
+
+// io.on('connection', (socket) => {
+//     console.log('User connected');
+
+// const room =rooms[Math.floor( Math.random() * 3)] //random index of arr 0 to 2
+// socket.join(room) 
+
+//     socket.on('disconnect', () => {
+//         console.log("User disconnected");
+//     })  //if we refresh the client ui its get disconnected and then get connected again....
+
+//     socket.send(`You are in ${room}`);  //ammiting it to every particular room 
+
+// io.sockets.in('Room 3').emit('room 3',`Congratulations ...You are in ${room}`) //we can ammit it to a particular room ! yep  this msg is only come when the client will join only room 3
+
+// });
+
+//the new one using new schema for room  and playing around it ...
+
+
+//creating three different rooms...
+const rooms =[ {"Room 1":0 } ,{"Room 2":0 },{"Room 3":0 }];  
 
 io.on('connection', (socket) => {
     console.log('User connected');
 
 const room =rooms[Math.floor( Math.random() * 3)] //random index of arr 0 to 2
-socket.join(room) 
 
-    socket.on('disconnect', () => {
+const roomName =Object.keys(room)[0]   
+
+socket.join(roomName);   
+        room[roomName] += 1 
+    socket.on('disconnect',() => {
+        room[roomName] -= 1
         console.log("User disconnected");
     })  //if we refresh the client ui its get disconnected and then get connected again....
 
-    socket.send(`You are in ${room}`);  //ammiting it to every particular room 
+    socket.send(`You are in ${roomName} & Total users  ${room[roomName]} `);  //ammiting it to every particular room 
 
-
-io.sockets.in('Room 3').emit('room 3',`Congratulations ...You are in ${room}`) //we can ammit it to a particular room ! yep  this msg is only come when the client will join only room 3
-
+io.sockets.in('Room 3').emit('room 3',`Congratulations ...You are in ${roomName}`) //we can ammit it to a particular room ! yep  this msg is only come when the client will join only room 3
 });
 
 
@@ -35,3 +59,5 @@ httpServer.listen(3000, () => {
     console.log('Server started');
 });
 
+
+ 
