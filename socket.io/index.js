2b402c270db1036c6e625c08eb99fbb8f9d03389
Kishukumar07 <kishukumars082@gmail.com>
2025-03-12 15:44:36 +0530

    created two nameSpaces and used it in place of io for dynamic emit

--- socket.io/index.js
+++ socket.io/index.js
@@ -11,7 +11,11 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-io.on('connection', (socket) => {
+
+const group1NS =io.of('/group1')//this is the first name space  now instead of io we'll use groupname-group1NS
+const group2NS =io.of('/group2')//simlarly for 2nd group (group2NameSpace)
+
+group1NS.on('connection', (socket) => {
     console.log('User connected');
 
     socket.on('disconnect', () => {
@@ -21,9 +25,22 @@ io.on('connection', (socket) => {
     socket.send("Welcome to my world!");
     socket.emit("newEvent", "This is a new Event "); //this is how we can create custom events as well ....
 
+});
 
+group2NS.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
+    })  //if we refresh the client ui its get disconnected and then get connected again....
+
+    socket.send("Welcome to my world!");
+    socket.emit("newEvent", "This is a new Event "); //this is how we can create custom events as well ....
 
 });
+
+
+
 
 httpServer.listen(3000, () => {
     console.log('Server started');
